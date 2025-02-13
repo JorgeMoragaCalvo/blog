0f1abf699b88997d63cb920cfe3154507e156850
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2025-02-13 17:40:54 -0300

    authenticate endpoint 2

--- src/main/java/com/globant/blog/services/AuthenticationService.java
+++ src/main/java/com/globant/blog/services/AuthenticationService.java
@@ -1,9 +1,7 @@
 package com.globant.blog.services;
 
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface AuthenticationService {
 
     UserDetails authenticate(String email, String password);
