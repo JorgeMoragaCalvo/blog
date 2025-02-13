3d4379ac940a045897e1344df5f79d6a1bbbd9eb
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2025-02-13 16:28:08 -0300

    delete endpoit implementation

--- src/main/java/com/globant/blog/services/CategoryService.java
+++ src/main/java/com/globant/blog/services/CategoryService.java
@@ -3,9 +3,11 @@ package com.globant.blog.services;
 import com.globant.blog.entities.CategoryEntity;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CategoryService {
 
     List<CategoryEntity> listCategories();
     CategoryEntity createCategory(CategoryEntity category);
+    void deleteCategory(UUID id);
 }
